c52bd82144af32c1e3aef7718fa92132e46a2c56
Tien Pham <tiendev94@gmail.com>
2019-05-12 18:37:22 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react"
 
 const App = () => {
+  const [name, setName] = useState("")
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <h3>{name}</h3>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          formSubmit()
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+      </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+const formSubmit = () => {
+  console.log("submit")
+}
+
+export default App
