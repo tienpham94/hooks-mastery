0fffe3b2134341b9369dd0c003c3f31c6fbe72a9
Tien Pham <tiendev94@gmail.com>
2019-05-14 21:14:07 +0300

    More context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useRef, createContext } from "react"
 
 import { useTitleInput } from "./hooks/useTitleInput"
+import Toggle from "./Toggle"
 
-const UserContext = createContext()
+export const UserContext = createContext()
 
 const App = () => {
   const [name, setName] = useTitleInput("")
   const ref = useRef()
 
   return (
-    <UserContext.Provider>
+    <UserContext.Provider
+      value={{
+        user: true
+      }}
+    >
       <div className="main-wrapper" ref={ref}>
         <h1 onClick={() => ref.current.classList.add("new-class")}>
           Level Up Dishes
         </h1>
         <h3>{name}</h3>
+        <Toggle />
         <form
           onSubmit={e => {
             e.preventDefault()
---
+++ src/Toggle.js
@@ -0,0 +1,18 @@
+import React, { useState, useContext } from "react"
+
+import { UserContext } from "./App"
+
+const Toggle = () => {
+  const [isToggled, setToggle] = useState(false)
+  const userInfo = useContext(UserContext)
+
+  if (!userInfo.user) return null
+  return (
+    <div>
+      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      {isToggled && <h2>Hello</h2>}
+    </div>
+  )
+}
+
+export default Toggle
