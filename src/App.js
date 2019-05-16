26fdc468cbe4d61d4a68cec1a1670bad5ec9a768
Tien Pham <tiendev94@gmail.com>
2019-05-16 20:34:51 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import Toggle from './Toggle';
-import Counter from './Counter';
-import { useTitleInput } from './hooks/useTitleInput';
+import React, { useMemo } from "react"
+import Toggle from "./Toggle"
+import Counter from "./Counter"
+import { useTitleInput } from "./hooks/useTitleInput"
 
 const App = () => {
-  const [name, setName] = useTitleInput('');
+  const [name, setName] = useTitleInput("")
+
+  const reverseWord = word => {
+    console.log("function call")
+    return word
+      .split("")
+      .reverse()
+      .join("")
+  }
+
+  const title = "Level Up Dishes"
+
+  const TitleReversed = useMemo(() => reverseWord(title), [title])
 
   return (
     <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+      <h1>{TitleReversed}</h1>
       <Toggle />
       <Counter />
       <form
         onSubmit={e => {
-          e.preventDefault();
+          e.preventDefault()
         }}
       >
         <input
@@ -24,7 +36,7 @@ const App = () => {
         <button>Submit</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
